f37d030785f3930ed64132836ef3ac2e6c7c3142
Bianca Antunes Passador <bia_antunes08@hotmail.com>
2022-10-03 12:26:24 -0300

    catch

--- src/main/java/br/com/dxc/cards/file/reader/FileWrite.java
+++ src/main/java/br/com/dxc/cards/file/reader/FileWrite.java
@@ -90,6 +90,8 @@ public class FileWrite {
 
         }catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
